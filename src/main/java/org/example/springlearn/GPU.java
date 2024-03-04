86ca90bfc1ce0372e2eb6979272cda637709db6f
Waranim <129619485+Waranim@users.noreply.github.com>
2024-03-04 23:48:38 +0500

    DI_v1.1.1

--- src/main/java/org/example/springlearn/GPU.java
+++ src/main/java/org/example/springlearn/GPU.java
@@ -1,10 +1,8 @@
 package org.example.springlearn;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class GPU implements MotherboardComponent {
 
     public GPU(){
